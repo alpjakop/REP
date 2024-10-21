2803b103e838e301e1522525436a3f32a630a341
Ezgi.Ozel <your-email@example.com>
2024-10-21 09:28:49 +0200

    One answer change

--- src/AssociativePropertyCheck.java
+++ src/AssociativePropertyCheck.java
@@ -4,37 +4,10 @@ public class AssociativePropertyCheck {
 
     public static void main(String[] args) {
         int trials = 1000000;
-        System.out.println("Checking associative property for doubles:");
-        checkAssociativePropertyForDouble(trials);
-
+        
         System.out.println("\nChecking associative property for floats:");
         checkAssociativePropertyForFloat(trials);
-
-        System.out.println("\nChecking associative property for ints:");
-        checkAssociativePropertyForInt(trials);
     }
-
-    private static void checkAssociativePropertyForDouble(int trials) {
-    int countEqual = 0;
-    Random random = new Random();
-
-    for (int i = 0; i < trials; i++) {
-        double x = random.nextDouble();
-        double y = random.nextDouble();
-        double z = random.nextDouble();
-
-        double leftSide = x + (y + z);  // Changed from (x + y) + z
-        double rightSide = (x + y) + z; // Changed from x + (y + z)
-
-        if (leftSide == rightSide) {
-            countEqual++;
-        }
-    }
-
-    double percentage = (countEqual / (double) trials) * 100;
-    System.out.printf("Percentage for double (modified order): %.6f%%\n", percentage);
-}
-
 
     private static void checkAssociativePropertyForFloat(int trials) {
         int countEqual = 0;
@@ -57,24 +30,4 @@ public class AssociativePropertyCheck {
         System.out.printf("Percentage for float (modified order): %.6f%%\n", percentage);
     }
 
-    private static void checkAssociativePropertyForInt(int trials) {
-        int countEqual = 0;
-        Random random = new Random();
-
-        for (int i = 0; i < trials; i++) {
-            int x = random.nextInt();
-            int y = random.nextInt();
-            int z = random.nextInt();
-
-            int leftSide = x + (y + z);
-            int rightSide = (x + y) + z;
-
-            if (leftSide == rightSide) {
-                countEqual++;
-            }
-        }
-
-        double percentage = (countEqual / (double) trials) * 100;
-        System.out.printf("Percentage for int (modified order): %.6f%%\n", percentage);
-    }
 }

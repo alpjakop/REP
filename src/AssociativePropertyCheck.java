9419581cb06ff6eb2bf75e4a5149a03fb014d950
alpjakop <alp.jakop@insa-rennes.fr>
2024-10-21 09:26:27 +0200

    Precision case studied

--- src/AssociativePropertyCheck.java
+++ src/AssociativePropertyCheck.java
@@ -7,38 +7,7 @@ public class AssociativePropertyCheck {
 
     public static void main(String[] args) {
         int trials = 1000000;
-        System.out.println("Checking associative property for doubles:");
-        checkAssociativePropertyForDouble(trials);
-
-        System.out.println("\nChecking associative property for floats:");
         checkAssociativePropertyForFloat(trials);
-
-        System.out.println("\nChecking associative property with precision limitation:");
-        checkAssociativePropertyWithPrecisionLimitation(trials);
-
-        System.out.println("\nChecking associative property for ints:");
-        checkAssociativePropertyForInt(trials);
-    }
-
-    private static void checkAssociativePropertyForDouble(int trials) {
-        int countEqual = 0;
-        Random random = new Random();
-
-        for (int i = 0; i < trials; i++) {
-            double x = random.nextDouble();
-            double y = random.nextDouble();
-            double z = random.nextDouble();
-
-            double leftSide = (x + y) + z;
-            double rightSide = x + (y + z);
-
-            if (leftSide == rightSide) {
-                countEqual++;
-            }
-        }
-
-        double percentage = (countEqual / (double) trials) * 100;
-        System.out.printf("Percentage for double: %.6f%%\n", percentage);
     }
 
     private static void checkAssociativePropertyForFloat(int trials) {
@@ -50,8 +19,13 @@ public class AssociativePropertyCheck {
             float y = random.nextFloat();
             float z = random.nextFloat();
 
-            float leftSide = (x + y) + z;
-            float rightSide = x + (y + z);
+            // Simulate precision  limitation
+            double xDouble = (double) x;
+            double yDouble = (double) y;
+            double zDouble = (double) z;
+
+            double leftSide = (xDouble + yDouble) + zDouble;
+            double rightSide = xDouble + (yDouble + zDouble);
 
             if (leftSide == rightSide) {
                 countEqual++;
@@ -59,54 +33,6 @@ public class AssociativePropertyCheck {
         }
 
         float percentage = (countEqual / (float) trials) * 100;
-        System.out.printf("Percentage for float: %.6f%%\n", percentage);
-    }
-
-    // New method simulating precision limitation (half precision)
-    private static void checkAssociativePropertyWithPrecisionLimitation(int trials) {
-        int countEqual = 0;
-        Random random = new Random();
-
-        for (int i = 0; i < trials; i++) {
-            double x = random.nextDouble();
-            double y = random.nextDouble();
-            double z = random.nextDouble();
-
-            // Simulate precision limitation
-            float xFloat = (float) x;
-            float yFloat = (float) y;
-            float zFloat = (float) z;
-
-            float leftSide = (xFloat + yFloat) + zFloat;
-            float rightSide = xFloat + (yFloat + zFloat);
-
-            if (leftSide == rightSide) {
-                countEqual++;
-            }
-        }
-
-        double percentage = (countEqual / (double) trials) * 100;
-        System.out.printf("Percentage with precision limitation: %.6f%%\n", percentage);
-    }
-
-    private static void checkAssociativePropertyForInt(int trials) {
-        int countEqual = 0;
-        Random random = new Random();
-
-        for (int i = 0; i < trials; i++) {
-            int x = random.nextInt();
-            int y = random.nextInt();
-            int z = random.nextInt();
-
-            int leftSide = (x + y) + z;
-            int rightSide = x + (y + z);
-
-            if (leftSide == rightSide) {
-                countEqual++;
-            }
-        }
-
-        double percentage = (countEqual / (double) trials) * 100;
-        System.out.printf("Percentage for int: %.6f%%\n", percentage);
+        System.out.printf("%.2f\n", percentage);
     }
 }
